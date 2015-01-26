ff1223e31a37088c6bb0502f5ea8577204471432
Gregorio Mongelli <gregorio.mongelli@acsone.eu>
2015-01-26 16:33:34 +0100

    GMO: update of the build date

--- arelle/Version.py
+++ arelle/Version.py
@@ -5,4 +5,4 @@ This module represents the time stamp when Arelle was last built
 (c) Copyright 2014 Mark V Systems Limited, All rights reserved.
 
 '''
-version = '2015-01-22 10:50 UTC'
+version = '2015-01-26 16:00 UTC'
