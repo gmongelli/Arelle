9c74827b4fae039210c2ac649bbc3e7f72541412
Gregorio Mongelli <gregorio.mongelli@acsone.eu>
2015-02-03 14:40:01 +0100

    [CHG]GMO: update version numbering for next release.

--- arelle/Version.py
+++ arelle/Version.py
@@ -5,4 +5,4 @@ This module represents the time stamp when Arelle was last built
 (c) Copyright 2014 Mark V Systems Limited, All rights reserved.
 
 '''
-version = '2015-01-29 17:25 UTC'
+version = '2015-02-03 12:00 UTC'
