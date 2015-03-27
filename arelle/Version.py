ba07bd7ab3415a35592cda2e8c82ea1e75cb0a73
Gregorio Mongelli <gregorio.mongelli@acsone.eu>
2015-03-27 18:36:23 +0100

    [ADD]GMO: preparatory work for fixing the performance problems with Tkinter.

--- arelle/Version.py
+++ arelle/Version.py
@@ -5,4 +5,4 @@ This module represents the time stamp when Arelle was last built
 (c) Copyright 2014 Mark V Systems Limited, All rights reserved.
 
 '''
-version = '2015-03-02 07:30 UTC'
+version = '2015-04-05 07:30 UTC'
