f740a31a30aa6948e3fb5dc82d921a679021def7
Gregorio Mongelli <gregorio.mongelli@acsone.eu>
2015-05-22 12:48:41 +0200

    [FIX]GMO: make the setup work for Python 3.4 and Tk/Tcl 8.6. There is a problem in cx_freeze: the environment variables TCL_LIBRARY and TK_LIBRARY are no longer correctly defined and thus Tk/Tcl does not work without them any longer.

--- buildWin64Dist.bat
+++ buildWin64Dist.bat
@@ -1,8 +1,10 @@
 rem Build Arelle GUI using cx_Freeze
 
-@set PYTHONDIR=c:\python32
+@set PYTHONDIR=c:\python34
 @set NSISDIR=C:\Program Files (x86)\NSIS
-@set BUILTDIR=build\exe.win-amd64-3.2
+@set BUILTDIR=build\exe.win-amd64-3.4
+@set TCL_LIBRARY=c:\python34\Lib\tcl8.6
+@set TK_LIBRARY=c:\python34\Lib\tcl8.6\tk8.6
 
 rem rmdir build /s/q
 rmdir dist /s/q
--- buildWin86Dist.bat
+++ buildWin86Dist.bat
@@ -1,8 +1,10 @@
 rem Build Arelle GUI using cx_Freeze
 
-@set PYTHONDIR=c:\python32x86
+@set PYTHONDIR=c:\python34x86
 @set NSISDIR=C:\Program Files (x86)\NSIS
-@set BUILTDIR=build\exe.win32-3.2
+@set BUILTDIR=build\exe.win32-3.4
+@set TCL_LIBRARY=c:\Python34x86\Lib\tcl8.6
+@set TK_LIBRARY=c:\Python34x86\Lib\tcl8.6\tk8.6
 
 rem rmdir build /s/q
 rmdir dist /s/q
