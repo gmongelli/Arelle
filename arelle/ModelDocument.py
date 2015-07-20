aebaea046d25d0752a5740f1399d3a13df58aafc
pczacsone <pierre.cavez@acsone.eu>
2015-07-20 17:39:25 +0200

    Fix Synchronisation between table cell and Properties tab no longer working (Bug #16317)

--- arelle/ModelDocument.py
+++ arelle/ModelDocument.py
@@ -677,8 +677,11 @@ class ModelDocument:
             self.isModified = False
     
     def close(self, visited=None, urlDocs=None):
-        if self.modelXbrl is not None:
-            self.modelXbrl = None
+        try:
+            if self.modelXbrl is not None:
+                self.modelXbrl = None
+        except:
+            pass
         if visited is None: visited = []
         visited.append(self)
         # note that self.modelXbrl has been closed/dereferenced already, do not use in plug in
