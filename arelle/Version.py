bbf3509a52e45f089171532cf20d4b5e72d2908b
pczacsone <pierre.cavez@acsone.eu>
2015-07-23 16:43:43 +0200

    change version to 1.6.4

--- arelle/Version.py
+++ arelle/Version.py
@@ -5,5 +5,5 @@ This module represents the time stamp when Arelle was last built
 (c) Copyright 2015 Mark V Systems Limited, All rights reserved.
 
 '''
-version = '2015-07-22 16:00 UTC'
-versionId = '1.6.3'
+version = '2015-07-23 17:00 UTC'
+versionId = '1.6.4'
