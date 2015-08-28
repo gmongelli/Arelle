27dbb61ad29b9303acd213d8f98118f87d00326d
pczacsone <pierre.cavez@acsone.eu>
2015-08-28 12:14:02 +0200

    fix a bug in a warning and save some space in case of rollup

--- arelle/UITkTable.py
+++ arelle/UITkTable.py
@@ -715,6 +715,9 @@ class XbrlTable(TkTableWrapper.Table):
                               hasLeftBorder=True, hasTopBorder=True,
                               hasRightBorder=True,
                               hasBottomBorder=not isRollUp)
+        if isRollUp:
+            # In this case, we can afford a reduced column width since the label can span over the other rolled up columns
+            self.tk.call(self._w, 'width', x, 3)
 
 
     def initCellSpan(self, x, y, colspan, rowspan):
@@ -844,9 +847,9 @@ class XbrlTable(TkTableWrapper.Table):
                 self.maxColumnWidths.fill(0)
                 self.maxRowHeights.fill(0)
         except Exception as err:
-            # Such exception may happen e.g. when quickly switching tables (things are apparently not thread safe)    
+            # Such exception may happen e.g. when quickly switching tables (things are apparently not thread safe)
             messagebox.showwarning(_("arelle - Error"),
-                        "Failed resize table:\n{1}".format(err),
+                        "Failed resize table:\n{0}".format(err),
                         parent=self.master.view.modelXbrl.modelManager.cntlr.parent)
             
 
