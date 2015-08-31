e73c5ca83cdf600f7f0602b972957dfb87cdde35
pczacsone <pierre.cavez@acsone.eu>
2015-08-31 15:20:47 +0200

    increment plugin version

--- arelle/plugin/EbaCompliance/__init__.py
+++ arelle/plugin/EbaCompliance/__init__.py
@@ -519,7 +519,7 @@ def fileOpenExtender(cntlr, menu):
 
 __pluginInfo__ = {
     'name': 'Improve EBA compliance of XBRL instances',
-    'version': '1.6',
+    'version': '1.7',
     'description': "This module extends the File menu and regenerates EBA filing indicators if needed and removes unused contexts and units.",
     'license': 'Apache-2',
     'author': 'Gregorio Mongelli (Acsone S. A.)',
